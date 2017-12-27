aea075d4c98dbdba48d8e06df462769562806296
Will Bolton <willyjayb@gmail.com>
2017-12-27 19:02:13 +0000

    Finish first draft of modifiation script + backup
    Interesting.
    
    The script has its quirks, such as inserting newlines into the output.
    Of course, these are straightforward to remove in vim, but there were a
    few other quirks I couldn't quite get my head around too.
    
    For example, in the corresponding script from the cited source, using
    line.strip() works fine, but if you remove the references to cnt then
    line seems to come through as an array, which I don't really understand.
    There are a lot of aspects of the script whose subtleties I glossed over
    though so I'm hardly surprised.
    
    Anyway, it seems to work so I'm going to test importing it and we'll
    make sure the transformed data is correct.
    
    Also add more current backup.

--- transform-file.py
+++ transform-file.py
@@ -1,3 +1,13 @@
+blank = ''
+leader = ''
+newleader = False
 with open('upgrade-paths.txt') as infile:
     for line in enumerate(infile):
-        print("{}".format(line[1].strip()))
+        if line[1].strip() == blank:
+            newleader = True
+        elif newleader == True:
+            leader = line[1]
+            print("{},{}".format(line[1].strip(),leader))
+            newleader = False
+        else:
+            print("{},{}".format(line[1].strip(),leader))
