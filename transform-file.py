5248ab6980abde1819e090b9dbb16a9405a020f7
Will Bolton <willyjayb@gmail.com>
2017-12-30 00:11:48 +0000

    Reformat class table and do more battle mechanics

--- transform-file.py
+++ transform-file.py
@@ -1,13 +1,26 @@
-blank = ''
-leader = ''
-newleader = False
-with open('upgrade-paths.txt') as infile:
+# blank = ''
+# leader = ''
+# newleader = False
+# with open('upgrade-paths.txt') as infile:
+#     for line in enumerate(infile):
+#         if line[1].strip() == blank:
+#             newleader = True
+#         elif newleader == True:
+#             leader = line[1]
+#             print("{},{}".format(line[1].strip(),leader))
+#             newleader = False
+#         else:
+#             print("{},{}".format(line[1].strip(),leader))
+import re
+
+sequence = 'start'
+sequencenumber = 1
+with open('class-ordering.csv') as infile:
     for line in enumerate(infile):
-        if line[1].strip() == blank:
-            newleader = True
-        elif newleader == True:
-            leader = line[1]
-            print("{},{}".format(line[1].strip(),leader))
-            newleader = False
+        splitline = re.split('\t',line[1])
+        if splitline[0] == sequence:
+            sequencenumber += 1
         else:
-            print("{},{}".format(line[1].strip(),leader))
+            sequence = splitline[0]
+            sequencenumber = 1
+        print("{}\t{}".format(line[1].strip(),sequencenumber))
