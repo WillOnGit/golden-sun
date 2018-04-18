79c0670af2d0c0e994ddd617bd8ef318170d36f6
Will Bolton <willyjayb@gmail.com>
2018-04-18 19:34:24 +0100

    Finish testing MySQL/Python connector
    Oh my god. This took far longer than it should have done.
    
    Getting used to the cursor object was a bit odd but not too
    time-consuming. Getting queries to execute and print the results was
    pretty much fine too, when the queries were simple strings.
    
    Getting substitution into the queries working? Nightmare.
    
    I took everything I needed from the MySQL/Python manual, section 5.4 or
    something, where they show retrieval using 2 dates in the query. I only
    ever tested with 1 and couldn't get it to bloody work, until I finally
    found a different manual page that (genuinely) helpfully highlights that
    you need to include a comma in Python to make something a tuple.
    
    Never mind, up and running now so can carry on working on main.

--- logic/mysql-test.py
+++ logic/mysql-test.py
@@ -2,11 +2,21 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user='gsuser', password='gsuser_pass', host='127.0.0.1', database='goldensun')
 
-test = cnx.cursor()
+#everything will look like this?
+testcrs = cnx.cursor()
 
-query = "SELECT * FROM DJINNI"
+query = "SELECT name, element, game FROM DJINNI WHERE element = %s"
 
-test.execute(query)
+element = "mercury"
 
-test.close()
-cnx.close()
+testcrs.execute(query, (element,))
+#testcrs.execute(oldquery)
+
+for (a,b,c) in testcrs:
+    #print a,b
+    #num += b
+    #print num
+    print("{} is a {} djinni from game {}".format(a, b, c))
+
+#testcrs.close()
+#cnx.close()
